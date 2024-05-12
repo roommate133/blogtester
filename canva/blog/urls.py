fad10033f0c40c87a4309cc804f2bba93229503d
roommate133 <qasimovaras@gmail.com>
2024-05-12 09:04:42 -0700

    ikinci commit

--- canva/blog/urls.py
+++ canva/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("blog/",views.blog,name="blog"),
     path("blog_detail",views.blog_detail,name="blog-detail"),
     path("article-form/",views.add_article,name="article-form"),
-    path("edit-article/<int:id",views.article_edit,name="edit-article"),
+    path("edit-article/<int:id>/",views.article_edit,name="edit-article"),
+    path("delete-article/<int:id>/",views.delete_article,name="delete-article"),
     path("example/",views.example,name="example"),
 ]
--- canva/blog/views.py
+++ canva/blog/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Article,Author,Tag
 from django.core.exceptions import PermissionDenied
 from .forms import ArticleForm
+from django.core.paginator import Paginator
 def blog(request):
     author_username=request.GET.get("author")
     authors=Article.objects.all()
     articles=Article.objects.all()
     tags=Tag.objects.all()
     tag_title=request.GET.get("tag")
+    page_number=int(request.GET.get('page',1))
     if author_username:
         articles=articles.filter(author__user__username=author_username)
     if tag_title:
         articles=articles.filter(tags__title=tag_title)
-    return render(request,"blog.html",context={'articles':articles,'authors':authors,'tags':tags})
+    paginator=Paginator(articles,3)
+    page=paginator.page(page_number)
+    articles=page.object_list
+    return render(request,"blog.html",context={'articles':articles,'authors':authors,'tags':tags,'paginator':paginator,'page':page})
 
 
 
@@ -45,12 +50,19 @@ def article_edit(request,id):
         form=ArticleForm(data=data,files=request.FILES,instance=article)
         if form.is_valid:
             form.save(request.user.author)
-            return redirect('blog:article-form')
+            return redirect('blog:add-article')
         return render(request,'article-form.html',{'form':form})
     else:
         form=Article(instance=article)
         return render(request,'article-form.html',{'form':form})
     
+
+def delete_article(request):
+    article=get_object_or_404(Article,id=id)
+    if article.author.user !=request.user:
+        raise PermissionDenied()
+    article.delete()
+    return redirect("blog:blog")
 
 def example(request):
     return render(request,"example.html")
